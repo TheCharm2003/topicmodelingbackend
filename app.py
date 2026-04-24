3ea4684cc7fdcb56056b1558bfb80e3442c79620
TheCharm2003 <114064618+TheCharm2003@users.noreply.github.com>
2026-04-24 12:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://topic-modeling-theta.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
